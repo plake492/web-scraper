f45a2babbc2c2481e7fb91587e0db9e3a3aedaf8
plake <plake492@gmail.com>
2019-10-06 23:49:35 -0400

    database setup, but very messy code

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // Dependencies
 const express = require("express");
 const exphbs = require("express-handlebars");
-// const mongojs = require("mongojs");
+const mongojs = require("mongojs");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 // Initialize Express
 const app = express();
@@ -22,30 +24,90 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-// app.get("/", function(req, res) {
-//   res.render("index", {
-//     list: results
-//   });
-// });
+//?==================================================//
+//*                    database                      //
+//?==================================================//
 
-// // Hook mongojs configuration to the db constiable
-// const db = mongojs(databaseUrl, collections);
-// db.on("error", function(error) {
-//   console.log("Database Error:", error);
-// });
+const results = [];
 
-// Main route (simple Hello World Message)
-// app.get("/", function(req, res) {
-//   res.render("Hello world");
-// });
+const db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
 
-// app.get("/", function(req, res) {
-//   res.render("index", {
-//     list: "HELLO"
-//   });
-// });
+app.get("/all", function(req, res) {
+  db.newsPosts.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+app.get("/scrape", function(req, res) {
+  results;
+  console.log("This is the result" + results);
+  // if (title && link && image && description) {
+  // Insert the data in the scrapedData db
+  db.newsPosts.insert(results, function(err, inserted) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(inserted);
+    }
+  });
+  res.send("Scrape Complete");
+});
+
+//!==================================================//
+//?==================================================//
+//*                    Handlebars                    //
+//?==================================================//
+
+axios
+  .get("https://www.nytimes.com/section/technology")
+  .then(function(responce) {
+    const $ = cheerio.load(responce.data);
+
+    $("div.css-1l4spti").each(function(i, element) {
+      const title = $(element)
+        .children()
+        .children("h2")
+        .text();
+      const image = $(element)
+        .children()
+        .children()
+        .children("figure")
+        .children()
+        .children("img")
+        .attr("src");
+      const description = $(element)
+        .children()
+        .children()
+        .text();
+      const link = $(element)
+        .children()
+        .attr("href");
+
+      results.push({
+        title,
+        image,
+        description,
+        link: `https://www.nytimes.com/${link}`
+      });
+    });
+    console.log(results);
+  });
+
+app.get("/", function(req, res) {
+  res.render("index", {
+    list: results
+  });
+});
+
 // require("./routes/hbs-routes.js")(app);
-require("./routes/hbs-routes")(app);
+// require("./routes/hbs-routes")(app);
 
 app.listen(3000, function() {
   console.log("App running on port 3000!");
