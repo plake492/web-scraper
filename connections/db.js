e816c64c0d0638d41a418e007397efaf2866362a
plake <plake492@gmail.com>
2019-10-08 11:43:47 -0400

    need to add mongoose

--- connections/db.js
+++ connections/db.js
@@ -1,11 +1,7 @@
-// const mongojs = require("mongojs");
-// const databaseUrl = "web-scraper-news";
-// const collections = ["newsPosts"];
+const databaseUrl = "web-scraper-news";
+const collections = ["newsPosts", "postComments"];
 
-// module.exports = function() {
-//   const db = mongojs(databaseUrl, collections);
-//   db.on("error", function(error) {
-//     console.log("Database Error:", error);
-//   });
-//   console.log(db);
-// };
+const db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
